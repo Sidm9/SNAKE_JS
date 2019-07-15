74b4af31b434a05e801e6836acbc034a27ab0cb7
SidM9 <43111742+Sidm9@users.noreply.github.com>
2019-07-16 00:24:55 +0530

    BOUNDRIES SET

--- nake.js
+++ nake.js
@@ -2,15 +2,48 @@ var canvas = document.getElementById("mycanvas");
 var ctx = canvas.getContext("2d");
 var x = canvas.width / 2;
 var y = canvas.height / 2;
-var dx = -7;
-var dy = -8;
-var foodx = 100;
+var dx = -2;
+var dy = -2;
+var foodx = 57;
 var foody = 100;
-var foodw = 50;
-var foodh = 50;
+var edge = 10;
+var foodh = 10;
+
+
+function getKeyAndMove(e){				
+    var key_code=e.which||e.keyCode;
+    switch(key_code){
+        case 37: //left arrow key
+            moveLeft();
+            break;
+        case 38: //Up arrow key
+            moveUp();
+            break;
+        case 39: //right arrow key
+            moveRight();
+            break;
+        case 40: //down arrow key
+            moveDown();
+            break;						
+    }
+}
+function moveLeft(){
+    dx = 1;
+}
+function moveUp(){
+    dy = -1;
+}
+function moveRight(){
+    dx = 1;
+}
+function moveDown(){
+    dy = 1;
+}
+
+
 function drawfood() {
     ctx.beginPath();
-    ctx.rect(foodx, foody, foodw, foodh);
+    ctx.rect(foodx, foody, 10, foodh);
     ctx.fillStyle = "red";
     ctx.fill();
     ctx.closePath();
@@ -20,7 +53,7 @@ function drawsnake() {
     //CLEAR PATH
     ctx.clearRect(0, 0, canvas.height, canvas.width);
     ctx.beginPath();
-    ctx.rect(x, y, 10, 10);
+    ctx.rect(foodx, y, edge, edge);
     ctx.fillStyle = "white";
     ctx.fill();
     ctx.closePath();
@@ -29,23 +62,17 @@ function drawsnake() {
 
     //COLLISION
 
-    if (x + dx > canvas.width || x + dx < 0) {
+    if (x + dx > canvas.width-edge || x + dx < 0-edge) {
         dx = -dx;
     }
-    if (y + dy > canvas.height || y + dy < 0) {
+    if (y + dy > canvas.height-edge || y + dy < 0-edge ) {
         dy = -dy;
     }
     //------------FOOD CONTACT COLLISION------------//
-  /*  if (y + dy < foody) {
+    if (y+ dy == foody && y+ dy == foodx ){
         dy = -dy;
     }
-    if (x + dx > foodw || x + dx < foodx) {
-        dx = -dx;
-    }
-    if (y + dy > foodh || y + dy < foody) {
-        dy = -dy;
-    }
-*/
+
 }
 
 function draw() {
@@ -53,4 +80,4 @@ function draw() {
     drawfood();
 }
 
-setInterval(draw, 20);
+setInterval(draw,5);
